5cdf892c54e3e49495fc6ef69f9af3ac24732313
Faizan <22cs10mo62@mitsgwl.ac.in>
2025-02-23 22:44:26 +0530

    add files

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oam8sxrdmyDZuFMh5d7',
+        issuer: 'https://dev-65809616.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
